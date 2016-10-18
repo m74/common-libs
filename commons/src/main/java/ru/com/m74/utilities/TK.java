25c7233fc49f2d6892ee006ba2ada2f87002d61f
Maxim Smirnov <mixam@h4y.ru>
2016-10-18 09:27:58 +0300

    fix errors

--- commons/src/main/java/ru/com/m74/utilities/TK.java
+++ commons/src/main/java/ru/com/m74/utilities/TK.java
@@ -1,28 +1,51 @@
 package ru.com.m74.utilities;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.Timestamp;
 import java.util.Collection;
 
 public class TK {
-	/**
-	 * Current timestamp without nanos.
-	 * 
-	 * @return
-	 */
-	public static Timestamp currentTimestamp() {
-		Timestamp now = new Timestamp(System.currentTimeMillis());
-		now.setNanos(0);
-		return now;
-	}
+    /**
+     * Конвертировать строку из iso8859-1 в utf8
+     *
+     * @param str
+     * @return
+     */
+    public static String iso2utf(String str) {
+        try {
+            return new String(str.getBytes("iso8859-1"), "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            return str;
+        }
+    }
 
-	public static boolean isEmpty(Object o) {
-		if (o instanceof Collection<?>)
-			return ((Collection<?>) o).isEmpty();
-		if (o instanceof String)
-			return ((String) o).isEmpty();
-		if (o instanceof Number)
-			return ((Number) o).intValue() == 0;
-		return o == null;
-	}
+    /**
+     * Current timestamp without nanos.
+     *
+     * @return
+     */
+    public static Timestamp currentTimestamp() {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        now.setNanos(0);
+        return now;
+    }
+
+
+    /**
+     * Пустой ли объект
+     *
+     * @param o
+     * @return
+     */
+
+    public static boolean isEmpty(Object o) {
+        if (o instanceof Collection<?>)
+            return ((Collection<?>) o).isEmpty();
+        if (o instanceof String)
+            return ((String) o).isEmpty();
+        if (o instanceof Number)
+            return ((Number) o).intValue() == 0;
+        return o == null;
+    }
 
 }
